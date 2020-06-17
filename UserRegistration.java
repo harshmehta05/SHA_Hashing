a376b0964602deaa685a0ad6319f51684526871f
harshmehta05 <41786609+harshmehta05@users.noreply.github.com>
2020-06-17 23:09:00 +0530

    User Registration Added

---
+++ UserRegistration.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class UserRegistration
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter username");
+    String username = sc.next();
+    System.out.println("Enter password");
+    String password = sc.next();
+  }
+}
